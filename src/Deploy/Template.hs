c6f9366ef27a39431396b85f67c59cf5ad7e0599
Ilya Zamaratskih <mrtstgdev@gmail.com>
2025-05-06 22:18:37 +0300

    feat: added new checks for vm and template name uniqueness

--- src/Deploy/Template.hs
+++ src/Deploy/Template.hs
@@ -3,6 +3,8 @@ module Deploy.Template (vmIDPresent) where
 import Api.Proxmox.Models.VM
 import qualified Data.Map as M
 
+-- looks up is VMID present in VMs map, used for
+-- checking, is templates existing
 vmIDPresent :: M.Map Int ProxmoxVM -> [Int] -> Either [Int] ()
 vmIDPresent vms = helper [] where
   helper :: [Int] -> [Int] -> Either [Int] ()
--- src/Deploy/VM.hs
+++ src/Deploy/VM.hs
@@ -7,25 +7,38 @@ module Deploy.VM
 
 import Data.Models.Config.VM (ConfigVM(..))
 import Data.Models.Config.Template
+import Utils (returnFirstDuplicate)
 
 type TemplateName = String
+type VMName = String
 
-data VMValidateError = TemplateIsNotPresent TemplateName
+data VMValidateError = TemplateIsNotPresent TemplateName | TemplateNameRepeat TemplateName | VMNameRepeat VMName
 
 instance Show VMValidateError where
-  show (TemplateIsNotPresent templateName) = "Template " <> templateName <> " is not listed in config file"
+  show (TemplateIsNotPresent templateName) = "Template " <> show templateName <> " is not listed in config file"
+  show (TemplateNameRepeat templateName) = "Template name " <> show templateName <> " is used two or more times"
+  show (VMNameRepeat vmName) = "VM name " <> show vmName <> " is used two or more times"
 
 validateVMsData :: [ConfigTemplate] -> [ConfigVM] -> Either VMValidateError ()
 validateVMsData templates vms = do
-  case vmTemplatesPresented templates vms of
-    Left templateName -> Left (TemplateIsNotPresent templateName)
-    Right _ -> return ()
+  () <- vmTemplatesPresented templates vms
+  () <- do
+    case returnFirstDuplicate (map configTemplateName templates) of
+      Nothing -> Right ()
+      (Just el) -> (Left . TemplateNameRepeat) el
+  () <- do
+    case returnFirstDuplicate (map configVMName vms) of
+      Nothing -> Right ()
+      (Just el) -> (Left . VMNameRepeat) el
+  return ()
 
-vmTemplatesPresented :: [ConfigTemplate] -> [ConfigVM] -> Either String ()
+-- checks, is templates presented on NAME level
+-- existence in proxmox node checked earlier
+vmTemplatesPresented :: [ConfigTemplate] -> [ConfigVM] -> Either VMValidateError ()
 vmTemplatesPresented templates = let
   templatesNames = map configTemplateName templates
-  helper :: [ConfigVM] -> Either String ()
+  helper :: [ConfigVM] -> Either VMValidateError ()
   helper [] = Right ()
   helper (RawVM {}:vms) = helper vms
-  helper (TemplatedConfigVM { .. }:vms) = if configVMParentTemplate `notElem` templatesNames then Left configVMParentTemplate else helper vms
+  helper (TemplatedConfigVM { .. }:vms) = if configVMParentTemplate `notElem` templatesNames then (Left . TemplateIsNotPresent) configVMParentTemplate else helper vms
   in helper
