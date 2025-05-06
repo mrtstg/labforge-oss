0302fc1eb465b9df27785ca5640d68bcac258fd5
Ilya Zamaratskih <mrtstgdev@gmail.com>
2025-05-06 22:18:13 +0300

    feat: new utils for finding duplicates

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,9 +1,16 @@
 module Utils 
   ( findExistingFile
   , defaultConfigFiles
+  , returnFirstDuplicate
   ) where
 
 import System.Directory
+
+returnFirstDuplicate :: (Eq a) => [a] -> Maybe a
+returnFirstDuplicate = helper [] where
+  helper :: (Eq a) => [a] -> [a] -> Maybe a
+  helper acc (el:els) = if el `elem` acc then Just el else helper (el:acc) els
+  helper _ [] = Nothing
 
 defaultConfigFiles :: [FilePath]
 defaultConfigFiles = ["proxmox-compose.yaml", "proxmox-compose.yml"]
