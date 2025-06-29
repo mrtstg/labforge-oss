c054a0ba6c906a15f2733b6858b2658bc878bab3
Ilya Zamaratskih <mrtstgdev@gmail.com>
2025-06-29 16:57:56 +0300

    feat: implemented delay field

--- src/Data/Models/Config/VM.hs
+++ src/Data/Models/Config/VM.hs
@@ -11,20 +11,24 @@ data ConfigVM = TemplatedConfigVM
   { configVMParentTemplate :: !String
   , configVMName :: !String
   , configVMID :: !(Maybe Int)
+  , configVMDelay :: !Int
   } | RawVM 
   { configVMName :: !String
   , configVMID :: !(Maybe Int)
-  } deriving Show
+  , configVMDelay :: !Int
+  } deriving (Show, Eq)
 
 instance FromJSON ConfigVM where
   parseJSON = withObject "ConfigVM" $ \v -> case KM.lookup "clone_from" v of
     Nothing -> RawVM 
       <$> v .: "name"
       <*> v .:? "vmid"
+      <*> v .:? "delay" .!= 0
     (Just (String _)) -> TemplatedConfigVM
       <$> v .: "clone_from"
       <*> v .: "name"
       <*> v .:? "vmid"
+      <*> v .:? "delay" .!= 0
     _anyOtherType -> fail "clone_from field has incorrect value type!"
 
 isTemplateVM :: ConfigVM -> Bool
