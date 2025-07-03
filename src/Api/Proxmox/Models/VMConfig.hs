881459a3e74bb417fddd146076a8a2c1406c5652
mrtstg <mrtstgdev@gmail.com>
2025-07-03 23:07:46 +0300

    refactor: used new field parser

--- src/Api/Proxmox/Models/VMConfig.hs
+++ src/Api/Proxmox/Models/VMConfig.hs
@@ -3,6 +3,7 @@ module Api.Proxmox.Models.VMConfig
   ( ProxmoxVMConfig(..)
   ) where
 
+import qualified Data.Aeson.KeyMap as KM
 import Data.Aeson
 
 -- detailed configuration, like in /nodes/{node}/qemu/{vmid}/config
@@ -17,5 +18,9 @@ instance FromJSON ProxmoxVMConfig where
   parseJSON = withObject "ProxmoxVMConfig" $ \v -> ProxmoxVMConfig
     <$> v .: "digest"
     <*> v .:? "name"
-    <*> v .:? "template" .!= False
-    <*> v .:? "lock"
+    <*> templateParser (KM.lookup "template" v)
+    <*> v .:? "lock" where
+      templateParser Nothing = pure False
+      templateParser (Just (Number 1)) = pure True
+      templateParser (Just (String "1")) = pure True
+      templateParser _ = pure False
