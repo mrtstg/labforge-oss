32801bbd4296e4dc39363e634196fc07c52b75ce
mrtstg <mrtstgdev@gmail.com>
2025-07-03 23:05:37 +0300

    feat: implemented new service type for api

--- src/Api/Proxmox/Models.hs
+++ src/Api/Proxmox/Models.hs
@@ -1,10 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Api.Proxmox.Models (ProxmoxResponse(..)) where
+module Api.Proxmox.Models 
+  ( ProxmoxResponse(..)
+  , NumericBoolWrapper(..)
+  ) where
 
 import Data.Aeson
+import Servant.API
 
 newtype ProxmoxResponse t = ProxmoxResponse t deriving Show
 
 instance (FromJSON t) => FromJSON (ProxmoxResponse t) where
   parseJSON = withObject "ProxmoxResponse" $ \v -> ProxmoxResponse
     <$> v .: "data"
+
+newtype NumericBoolWrapper = NumericBoolWrapper Bool deriving (Show, Eq)
+
+instance ToHttpApiData NumericBoolWrapper where
+  toQueryParam (NumericBoolWrapper True) = "1"
+  toQueryParam (NumericBoolWrapper False) = "0"
