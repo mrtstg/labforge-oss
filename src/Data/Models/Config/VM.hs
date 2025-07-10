3ff02b79be2d7884e3b97fb374dde86f2ff2b1db
mrtstg <mrtstgdev@gmail.com>
2025-07-10 22:10:01 +0300

    feat: added field in config file for defining networks

--- src/Data/Models/Config/VM.hs
+++ src/Data/Models/Config/VM.hs
@@ -1,17 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.Models.Config.VM 
   ( ConfigVM(..)
+  , ConfigVMNetwork(..)
   , isTemplateVM
   ) where
 
+import Api.Proxmox.Models.NetworkInterface
 import Data.Aeson
 import qualified Data.Aeson.KeyMap as KM
+import qualified Data.Text as T
+
+data ConfigVMNetwork = ConfigVMNetwork
+  { configVMNetworkName :: !String
+  , configVMNetworkFirewall :: !Bool
+  , configVMDeviceType :: !NetworkInterfaceType
+  , configVMNetworkTag :: !(Maybe String)
+  , configVMNetworkNumber :: !(Maybe Int)
+  } deriving (Show, Eq)
+
+instance FromJSON ConfigVMNetwork where
+  parseJSON (String networkName) = pure $ ConfigVMNetwork (T.unpack networkName) True VIRTIO Nothing Nothing
+  parseJSON (Object v) = ConfigVMNetwork 
+    <$> v .: "name"
+    <*> v .:? "firewall" .!= True
+    <*> v .:? "type" .!= VIRTIO
+    <*> v .:? "tag"
+    <*> v .:? "number"
+  parseJSON _ = error "ConfigVMNetwork has invalid type"
 
 data ConfigVM = TemplatedConfigVM 
   { configVMParentTemplate :: !String
   , configVMName :: !String
   , configVMID :: !(Maybe Int)
   , configVMDelay :: !Int
+  , configVMNetworks :: !(Maybe [ConfigVMNetwork])
   } | RawVM 
   { configVMName :: !String
   , configVMID :: !(Maybe Int)
@@ -29,6 +51,7 @@ instance FromJSON ConfigVM where
       <*> v .: "name"
       <*> v .:? "vmid"
       <*> v .:? "delay" .!= 0
+      <*> v .:? "networks" .!= Nothing
     _anyOtherType -> fail "clone_from field has incorrect value type!"
 
 isTemplateVM :: ConfigVM -> Bool
