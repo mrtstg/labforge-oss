52c12a310a294b53dfe77bb87f2c9171f401ddcf
mrtstg <mrtstgdev@gmail.com>
2025-07-12 15:41:51 +0300

    refactor: changed position of monad arguments for simplify usage

--- src/Api/Retry.hs
+++ src/Api/Retry.hs
@@ -4,12 +4,13 @@ module Api.Retry
   , defaultRetryClient
   , defaultRetryClient'
   , waitForClient
+  , retryClient'
   ) where
 
-import System.Log.Logger
-import Control.Concurrent
-import Servant.Client
-import Api.Proxmox
+import           Api.Proxmox
+import           Control.Concurrent
+import           Servant.Client
+import           System.Log.Logger
 
 type RetryAmount = Int
 type RetryTimeout = Int
@@ -20,18 +21,21 @@ defaultRetryClient' :: ProxmoxState -> ClientM a -> IO (Either ClientError a)
 defaultRetryClient' (ProxmoxState url manager') = defaultRetryClient (mkClientEnv manager' url)
 
 defaultRetryClient :: ClientEnv -> ClientM a -> IO (Either ClientError a)
-defaultRetryClient env res = retryClient env res 5 1_500_000
+defaultRetryClient env = retryClient env 5 1_500_000
 
-retryClient :: ClientEnv -> ClientM a -> RetryAmount -> RetryTimeout -> IO (Either ClientError a)
-retryClient env res 0 _ = runClientM res env
-retryClient env res retryA retryT = do
+retryClient' :: ProxmoxState -> RetryAmount -> RetryTimeout -> ClientM a -> IO (Either ClientError a)
+retryClient' (ProxmoxState url manager') = retryClient (mkClientEnv manager' url)
+
+retryClient :: ClientEnv -> RetryAmount -> RetryTimeout -> ClientM a -> IO (Either ClientError a)
+retryClient env 0 _ res = runClientM res env
+retryClient env retryA retryT res = do
   res' <- runClientM res env
   case res' of
     (Left e) -> do
       warningM loggerName "Got API request error, retrying..."
       debugM loggerName $ "Retrying client error: " <> show e
       threadDelay retryT
-      retryClient env res (retryA - 1) retryT
+      retryClient env (retryA - 1) retryT res
     (Right v) -> (pure . Right) v
 
 type FailureMessage = String
@@ -43,7 +47,7 @@ waitForClient maxRetryT failMessage retryA retryT' v f = do
   res' <- v
   case res' of
     (Left e) -> if retryA <= 1 then (pure . Left) e else do
-      errorM loggerName "Error during API request. Retry..."
+      warningM loggerName "Error during API request. Retry..."
       threadDelay retryT
       waitForClient maxRetryT failMessage (retryA - 1) retryT v f
     (Right res) -> do
