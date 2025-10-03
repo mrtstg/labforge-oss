312f9a75b2a7392786a756e80e5d9c73eaf412ba
mrtstg <mrtstgdev@gmail.com>
2025-10-03 15:47:42 +0300

    feat: implemented api for jobservice api

---
+++ jobservice-api/src/Api/Jobservice.hs
@@ -0,0 +1,65 @@
+{- Copyright (C) 2025 Ilya Zamaratskikh
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses>. -}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeOperators       #-}
+module Api.Jobservice
+  ( jobserviceServer
+  ) where
+
+import           Api.Keycloak.Models
+import           Auth.Token
+import           Config
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Control.Monad.Reader
+import           Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Text                  as T
+import           Jobservice.Models
+import           Jobservice.Schema
+import           Models.JSONError
+import           Network.AMQP
+import           Redis.Common
+import           Servant
+
+jobserviceServer :: ServerT JobserviceAPI AppT
+jobserviceServer = sendMessage :<|> getHeldImages
+
+sendMessage :: JobserviceMessage -> BearerWrapper -> AppT ()
+sendMessage msgPayload (BearerWrapper token) = do
+  _ <- requireRealmRoles token ["jobservice-send"]
+  -- TODO: future validation
+  r <- asks rabbitConnection
+  chan <- liftIO $ openChannel r
+  let msg = newMsg { msgBody = encode msgPayload, msgDeliveryMode = Just NonPersistent }
+  _ <- liftIO $ publishMsg chan "jobserviceExchange" "" msg
+  pure ()
+
+getHeldImages :: BearerWrapper -> AppT [String]
+getHeldImages (BearerWrapper token) = do
+  _ <- requireManyRealmRoles token [["image-view"], ["image-admin"]]
+  redisV <- getValue' jobserviceUsedImagesKey
+  case redisV of
+    Nothing        -> sendJSONError err404 (JSONError "notFound" "" Null)
+    (Just payload) -> do
+      case eitherDecode (LBS.fromStrict payload) of
+        (Left e) -> do
+          $(logError) $ "Held images decode error: " <> (T.pack . show) e
+          sendJSONError err500 (JSONError "internalError" "Failed to decode data" Null)
+        (Right (images :: [String])) -> pure images
---
+++ jobservice-api/src/App/Commands.hs
@@ -0,0 +1,119 @@
+{- Copyright (C) 2025 Ilya Zamaratskikh
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses>. -}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+module App.Commands (runCommand) where
+
+import           Api.Keycloak.Token
+import           App
+import           App.Types
+import           Auth.Token
+import           Config
+import           Control.Concurrent
+import           Control.Concurrent.Async
+import           Control.Exception
+import           Control.Monad               (void, when)
+import           Control.Monad.Logger
+import           Control.Monad.Reader
+import           Data.Aeson
+import           Data.ByteString.Char8       (ByteString)
+import qualified Data.ByteString.Lazy.Char8  as BLS
+import           Data.Maybe
+import           Data.Pool                   (Pool)
+import           Data.Text                   (pack)
+import           Database
+import           Database.Persist.Postgresql
+import           Database.Persist.Sqlite
+import           Jobservice.Models
+import           Network.AMQP
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Logger
+import           Redis.Common
+import           Redis.Environment
+import           Servant.Client
+import           Service.Config
+import           System.Environment
+import           System.Exit
+
+createPool :: Bool -> ByteString -> IO (Pool SqlBackend)
+createPool debug url = do
+  (if debug then runStdoutLoggingT else flip runLoggingT (\_ _ _ _ -> pure ())) $ createPostgresqlPool url 10
+
+f :: Config -> IO ()
+f cfg = forever $ do
+  catch (void $ appTIO inner cfg) err
+  threadDelay 2000000
+  where
+  err :: SomeException -> IO ()
+  err _ = pure ()
+
+  inner :: AppT ()
+  inner = do
+    $(logInfo) "Checking images key"
+    v <- getValue' jobserviceUsedImagesKey
+    case v of
+      (Just _) -> $(logInfo) "Images key found"
+      Nothing -> do
+        $(logInfo) "Creating images request"
+        r <- asks rabbitConnection
+        chan <- liftIO $ openChannel r
+        let msg = newMsg { msgBody = encode JobserviceUpdateUsedImages {}, msgDeliveryMode = Just NonPersistent }
+        _ <- liftIO $ publishMsg chan "jobserviceExchange" "" msg
+        liftIO $ threadDelay 5000000
+
+runCommand :: AppOpts -> IO ()
+runCommand AppOpts { debugOn=debug, appCommand=MakeMigrations } = do
+  let logFunction = if debug then defaultLogF else filterLogF LevelInfo
+  url <- runLoggingT requirePostgresString logFunction
+  pool <- createPool debug url
+  runSqlPool doMigration pool
+runCommand AppOpts { debugOn=debug, appCommand=RunServerOn port runMigrate } = do
+  let logFunction = if debug then defaultLogF else filterLogF LevelInfo
+  url <- runLoggingT requirePostgresString logFunction
+  pool <- createPool debug url
+  when runMigrate $ runSqlPool doMigration pool
+
+  redisC <- redisConnectionFromEnv
+  when (isNothing redisC) $ do
+    flip runLoggingT logFunction $ $(logError) "Failed to init redis connection"
+    exitWith (ExitFailure 1)
+
+  _ <- do
+    e <- getEnvironment
+    flip runLoggingT logFunction $ $(logDebug) $ "Current environment: " <> (pack . show) e
+
+  creds <- runLoggingT requireKeycloakClient logFunction
+  tokenV <- createTokenVar
+  (authUrl, authManager) <- runLoggingT (requireServiceEnv "AUTH") logFunction
+
+  amqpConn <- runLoggingT (requireRabbitMQCreds openConnection') logFunction
+
+  let config = Config { serviceCredentials=creds
+    , logFunction=logFunction
+    , configDBPool=pool
+    , authToken=tokenV
+    , authEnv=mkClientEnv authManager authUrl
+    , authFunctions=genericTokenFunctions logFunction creds (mkClientEnv authManager authUrl)
+    , redisConnection=fromJust redisC
+    , rabbitConnection = amqpConn
+    }
+  let app' = app config
+
+  _ <- async $ f config
+
+  _ <- flip runLoggingT logFunction $ $(logInfo) "Starting server!"
+  withStdoutLogger $ \aplogger -> do
+    let settings = setPort port $ setLogger aplogger defaultSettings
+    runSettings settings app'
