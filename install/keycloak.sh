0ae750784f9a834db398915b954f81cacbbec8d8
mrtstg <mrtstgdev@gmail.com>
2025-11-03 14:49:12 +0300

    feat: added new role for instances patch

--- install/keycloak.sh
+++ install/keycloak.sh
@@ -19,6 +19,7 @@ REALM_ROLES=(
     "jobservice-send"
     "creator-minimal"
     "deployment-alloc"
+    "deployment-instance-admin"
 )
 REALM_CLIENTS=(
     "cluster-manager"
@@ -37,7 +38,7 @@ USERS_ROLES=(
     "validate-users,deployment-admin"
     ""
     "validate-users,role-read,group-read,role-manage,user-read,realm-management.manage-realm,realm-management.view-realm,realm-management.query-realms,realm-management.realm-admin"
-    "deployment-admin,image-view,deployment-alloc"
+    "deployment-admin,image-view,deployment-alloc,deployment-instance-admin"
     "jobservice-send,validate-users"
 )
 
