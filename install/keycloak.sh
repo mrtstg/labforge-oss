bb0fe0414b35951e0eda2e7abe09ea8a3f3db2cb
mrtstg <mrtstgdev@gmail.com>
2025-11-04 12:09:13 +0300

    feat: added missing role for jobservice executor

--- install/keycloak.sh
+++ install/keycloak.sh
@@ -38,7 +38,7 @@ USERS_ROLES=(
     "validate-users,deployment-admin"
     ""
     "validate-users,role-read,group-read,role-manage,user-read,realm-management.manage-realm,realm-management.view-realm,realm-management.query-realms,realm-management.realm-admin"
-    "deployment-admin,image-view,deployment-alloc,deployment-instance-admin"
+    "deployment-admin,image-view,deployment-alloc,deployment-instance-admin,jobservice-send"
     "jobservice-send,validate-users"
 )
 
