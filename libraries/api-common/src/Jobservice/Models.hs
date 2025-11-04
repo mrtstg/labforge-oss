afa172820abd7c0db5113ff49be127979c6cff2a
mrtstg <mrtstgdev@gmail.com>
2025-11-04 11:40:22 +0300

    feat: added new jobservice message types

--- libraries/api-common/src/Jobservice/Models.hs
+++ libraries/api-common/src/Jobservice/Models.hs
@@ -12,15 +12,23 @@ import           Data.Text         (Text)
 jobserviceUsedImagesKey = "jobservice-used-images"
 
 data JobserviceMessage = JobserviceUpdateUsedImages {}
-                       | JobserviceAllocateNode { deploymentId :: !Text } deriving (Show, Eq)
+                       | JobserviceAllocateNode { deploymentId :: !Text }
+                       | JobserviceDeployInstance { deploymentId :: !Text }
+                       | JobserviceDestroyInstance { deploymentId :: !Text } deriving (Show, Eq)
 
 instance ToJSON JobserviceMessage where
   toJSON (JobserviceUpdateUsedImages {}) = object ["type" .= String "updateImages"]
   toJSON (JobserviceAllocateNode { .. }) = object ["type" .= String "allocateNode", "deploymentId" .= deploymentId]
+  toJSON (JobserviceDeployInstance { .. }) = object ["type" .= String "deployInstance", "deploymentId" .= deploymentId]
+  toJSON (JobserviceDestroyInstance { .. }) = object ["type" .= String "destroyInstance", "deploymentId" .= deploymentId]
 
 instance FromJSON JobserviceMessage where
   parseJSON = withObject "JobserviceMessage" $ \v -> case KM.lookup "type" v of
     (Just (String "updateImages")) -> pure JobserviceUpdateUsedImages {}
     (Just (String "allocateNode")) -> JobserviceAllocateNode
+      <$> v .: "deploymentId"
+    (Just (String "deployInstance")) -> JobserviceDeployInstance
+      <$> v .: "deploymentId"
+    (Just (String "destroyInstance")) -> JobserviceDestroyInstance
       <$> v .: "deploymentId"
     _anyOther                      -> fail "Invalid task type!"
