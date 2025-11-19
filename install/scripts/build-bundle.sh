b6b624aaf92ab251293e9c73283252711297ac6d
mrtstg <mrtstgdev@gmail.com>
2025-11-19 17:13:05 +0300

    feat: added full bundle script

---
+++ install/scripts/build-bundle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+make build-nginx-prod
+make build-websockify
+make build-lib-image
+make build-lib-bin-image
+make build-images
+make build-fs-agent
+make -C proxmox-compose build
+make save-images
+make bundle
