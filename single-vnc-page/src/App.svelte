6a08d1e81b4b3005cfa7c092a3ed4ef37fe7c907
mrtstg <mrtstgdev@gmail.com>
2026-01-17 19:43:26 +0300

    refactor: removed access requests due to server check of this

--- single-vnc-page/src/App.svelte
+++ single-vnc-page/src/App.svelte
@@ -13,28 +13,6 @@
   //const port = window.location.protocol == "https:" ? "443" : "80";
   const proto = window.location.protocol == "https:" ? "wss" : "ws";
   const url = proto + "://" + host + "/api/vm/" + vmPort + "/vnc";
-
-  let errorsRow = 0;
-  let exceptionsRow = 0;
-  setInterval(() => {
-    if (errorsRow == 5) {
-      window.location.reload()
-    }
-    if (exceptionsRow == 10) {
-      window.location.reload()
-    }
-    if (vmPort != null) {
-      fetch("/api/deployment/vmport/access", { headers: { "X-VM-PORT": vmPort } }).then(resp => {
-        if (resp.status >= 200 && resp.status <= 300) {
-          errorsRow = 0;
-        } else if (resp.status >= 400 && resp.status < 500) {
-          errorsRow++;
-        } else if (resp.status >= 500) {
-          exceptionsRow++;
-        }
-      })
-    }
-  }, 5000)
 </script>
 
 {#if vmPort != null}
