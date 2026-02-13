3919358186cde035577dc423b3137e4a312593ba
mrtstg <mrtstgdev@gmail.com>
2026-02-13 14:09:59 +0300

    fix: fixed adding event listeners to all dropdowns

--- frontend-server/static/js/navbar.js
+++ frontend-server/static/js/navbar.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     navbar.classList.toggle('is-active')
     target.classList.toggle('is-active')
   })
-  var dropdown = document.querySelector('.dropdown');
-  dropdown.addEventListener('click', function(event) {
+  var dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns.forEach(dropdown => dropdown.addEventListener('click', function(event) {
      event.stopPropagation();
      dropdown.classList.toggle('is-active');
-  });
+  }));
 });
