28fc0babaca2016b02907049c444d640ef2ab31e
mrtstg <mrtstgdev@gmail.com>
2026-04-24 20:41:30 +0300

    fix: fixed vmid url parse

--- single-vnc-page/src/App.svelte
+++ single-vnc-page/src/App.svelte
@@ -3,14 +3,14 @@
 
   let vmPort: string | null = null
   const parsedURL = new URL(document.URL)
-  const vmPortMatch = parsedURL.pathname.match("\/vnc\/(.*)")
+  const vmPortMatch = parsedURL.pathname.match("\/vnc\/([^\/]*)")
+  const isFull = window.location.href.endsWith("/full")
 
   if (vmPortMatch != null && vmPortMatch.length > 1) {
     vmPort = vmPortMatch[1]
   }
 
   const host = window.location.hostname;
-  //const port = window.location.protocol == "https:" ? "443" : "80";
   const proto = window.location.protocol == "https:" ? "wss" : "ws";
   const url = proto + "://" + host + "/api/vm/" + vmPort + "/vnc";
 </script>
@@ -18,5 +18,6 @@
 {#if vmPort != null}
   <NoVnc
     url={url}
+    showConnectionState={!isFull}
   />
 {/if}
