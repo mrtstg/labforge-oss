c2c41bff4df3067f4ae2fb1fc8e3d366fdc11b18
mrtstg <mrtstgdev@gmail.com>
2026-04-24 21:03:17 +0300

    feat: added SETNX-based redis locks

--- libraries/redis-utils/src/Redis/Lock.hs
+++ libraries/redis-utils/src/Redis/Lock.hs
@@ -1,22 +1,44 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 module Redis.Lock
   ( checkRedisLock
   , redisLockWrapper
   , redisRateLockWrapper
   , redisLockWrapper_
+  , redisNXLockWrapper
   ) where
 
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
 import           Control.Monad.Reader
+import qualified Data.ByteString.Char8  as BS
 import           Data.Functor           ((<&>))
 import           Data.Maybe
 import           Data.Text              (pack)
+import           Database.Redis
 import           Redis.Common
 
 checkRedisLock :: (MonadLogger m, MonadIO m, RedisConnection a, MonadReader a m) => String -> m Bool
 checkRedisLock k = ($(logDebug) . pack $ "Checking lock key " <> k) >> getValue' k <&> isNothing
+
+redisNXLockWrapper :: (MonadLogger m, MonadIO m, RedisConnection a, MonadReader a m) => String -> Integer -> IO Integer -> m r -> m r -> m r
+redisNXLockWrapper lockKey lockTime getTime onLocked onFree = do
+  $(logDebug) . pack $ "Trying NX key " <> lockKey
+  ts <- liftIO $ getTime <&> \x -> x + lockTime
+  redis <- asks getRedisConnection
+  r <- (liftIO . runRedis redis) (sendRequest (map BS.pack ["SET", lockKey, show ts, "NX", "EX", show lockTime]))
+  case r of
+    (Left (Bulk Nothing)) -> do
+      $(logError) . pack $ "NX key " <> lockKey <> " is taken"
+      onLocked
+    (Right Ok) -> do
+      x <- onFree
+      $(logDebug) . pack $ "Unlocking key " <> lockKey
+      (_ :: Either Reply Integer) <- (liftIO . runRedis redis) $ eval "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end" [BS.pack lockKey] [BS.pack . show $ ts]
+      pure x
+    _otherStatus -> ($(logWarn) . pack $ "Unknown status: " <> show _otherStatus) >> onLocked
 
 redisLockWrapper_ :: (MonadLogger m, MonadIO m, RedisConnection a, MonadReader a m) => String -> Integer -> m b -> m c -> m ()
 redisLockWrapper_ lockKey lockTime onLocked onFree = do
