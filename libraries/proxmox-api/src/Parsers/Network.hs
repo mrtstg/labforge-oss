8a9651a40cc194950b9bd3988a4ebf10b8bbd87f
mrtstg <mrtstgdev@gmail.com>
2026-07-10 03:16:39 +0300

    feat: implemented network change config parser

--- libraries/proxmox-api/src/Parsers/Network.hs
+++ libraries/proxmox-api/src/Parsers/Network.hs
@@ -1,9 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Parsers.Network (parseNetworkDevice) where
+module Parsers.Network (parseNetworkDevice, parseChangesConfig, interfacePendingState) where
 
 import           Data.Attoparsec.Text
 import qualified Data.Map             as M
-import           Data.Text
+import           Data.Text            (Text)
+
+data ChangesLine = OtherLine String | InterfaceAdded String | InterfaceRemoved String deriving (Show, Eq)
+
+interfacePendingState :: Text -> String -> Either String Bool
+interfacePendingState changesConfig interfaceName = do
+  parseRes <- parseChangesConfig changesConfig
+  let isDeleted = InterfaceRemoved interfaceName `elem` parseRes
+  let isAdded = InterfaceAdded interfaceName `elem` parseRes
+  case (isDeleted, isAdded) of
+    (True, _) -> pure False
+    (False, True) -> pure True
+    _anyOther -> Left $ "Interface " <> interfaceName <> " did not changed state."
+
+parseChangesConfig :: Text -> Either String [ChangesLine]
+parseChangesConfig = parseOnly (manyTill (choice [interfaceLineParser, otherLineParser]) endOfInput)
+
+interfaceLineParser :: Parser ChangesLine
+interfaceLineParser = do
+  sym <- satisfy (`elem` ['+', '-'])
+  _ <- string "iface"
+  _ <- many1 space
+  iface <- manyTill anyChar space
+  _ <- manyTill anyChar endOfLine
+  pure $ (if sym == '+' then InterfaceAdded else InterfaceRemoved) iface
+
+otherLineParser :: Parser ChangesLine
+otherLineParser = do
+  line <- manyTill anyChar endOfLine
+  pure $ OtherLine line
 
 parseNetworkDevice :: Text -> Either String (M.Map String String)
 parseNetworkDevice = fmap M.fromList . parseOnly networkArgsParser
