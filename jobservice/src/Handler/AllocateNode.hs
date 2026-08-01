0703d16e4e5534f4e01f22e6f72fa87d85e69b9b
mrtstg <mrtstgdev@gmail.com>
2026-08-01 13:07:31 +0300

    feat: added comments to linux bridges

--- jobservice/src/Handler/AllocateNode.hs
+++ jobservice/src/Handler/AllocateNode.hs
@@ -59,7 +59,7 @@ renameNet namesMap vmData@(TemplatedConfigVM { configVMNetworks = Just nets }) =
     Nothing  -> d
     (Just v) -> d { configVMNetworkName = v }
 
-renameNetworks :: M.Map String String -> [ConfigNetwork] -> (String -> ConfigNetwork) -> (String -> ConfigNetwork) -> [ConfigNetwork]
+renameNetworks :: M.Map String String -> [ConfigNetwork] -> (String -> ConfigNetwork) -> (String -> String -> ConfigNetwork) -> [ConfigNetwork]
 renameNetworks namesMap networks sdnConstructor bridgeConstructor = f [] networks where
   f :: [ConfigNetwork] -> [ConfigNetwork] -> [ConfigNetwork]
   f acc [] = acc
@@ -68,7 +68,7 @@ renameNetworks namesMap networks sdnConstructor bridgeConstructor = f [] network
     (Just newName) -> do
       case net of
         (SDNNetwork {})        -> f (sdnConstructor newName:acc) nets
-        (BridgeNetwork {})     -> f (bridgeConstructor newName:acc) nets
+        (BridgeNetwork {})     -> f (bridgeConstructor (configNetworkName net) newName:acc) nets
         a@(ExistingNetwork {}) -> f (a:acc) nets
 
 allocateNode :: (Envelope, Message) -> JobserviceMessageMeta -> AppT ()
@@ -140,7 +140,7 @@ allocateNode (env, msg) m@(JobserviceMessageMeta { .. }) = do
                             $(logInfo) $ "[" <> deploymentId <> "] Allocated networks"
                             sdnZone <- asks (T.unpack . deploySDNZone)
                             let namesMap = M.mapKeys T.unpack . M.fromList $ zip (map T.pack networkToRename) networksNames
-                            let networks = renameNetworks namesMap templateNetworks (\n -> SDNNetwork {configNetworkZone=sdnZone, configNetworkVLANAware=Nothing, configNetworkSubnets=[], configNetworkName=n}) (\n -> BridgeNetwork {configNetworkComments=T.unpack $ userTag <> ";" <> templateTitle, configNetworkAutostart=True, configNetworkName=n})
+                            let networks = renameNetworks namesMap templateNetworks (\n -> SDNNetwork {configNetworkZone=sdnZone, configNetworkVLANAware=Nothing, configNetworkSubnets=[], configNetworkName=n}) (\oldN n -> BridgeNetwork {configNetworkComments=T.unpack $ T.pack oldN <> "@" <> templateTitle <> "|" <> userTag, configNetworkAutostart=True, configNetworkName=n})
                             let replacedNetworksVM = map (renameNet namesMap) templateVMs
                             displayAllocRes'' <- withTokenVariable $ \t -> do
                               defaultRetryClientC deploymentEnv (D.requestDeploymentDisplay nodeName deploymentId (Just $ length templateVMs) (BearerWrapper t))
